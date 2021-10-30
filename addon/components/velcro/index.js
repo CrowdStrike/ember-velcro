1398f89a9586b901248e85490a6427df67a4e50c
Cam Skene <camskene@gmail.com>
2021-10-30 18:36:27 -0400

    feat: handle targetElement or popoverElement resize

--- addon/components/velcro/index.js
+++ addon/components/velcro/index.js
@@ -1,24 +1,35 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { createPopper } from '@popperjs/core';
+import { inject as service } from '@ember/service';
 import { modifier } from 'ember-modifier';
 
 export default class VelcroComponent extends Component {
+  @service resizeObserver;
+
   _targetElement = undefined;
   _popoverElement = undefined;
+  _popper = undefined;
 
   velcroTarget = modifier((element) => {
     this._targetElement = element;
+    this.resizeObserver.observe(element, this._updatePopper);
   });
 
   velcro = modifier((element) => {
     this._popoverElement = element;
 
     this._createPopper();
+    this.resizeObserver.observe(element, this._updatePopper);
   });
 
   @action
   _createPopper() {
-    createPopper(this._targetElement, this._popoverElement);
+    this._popper = createPopper(this._targetElement, this._popoverElement);
+  }
+
+  @action
+  _updatePopper() {
+    this._popper.update();
   }
 }
