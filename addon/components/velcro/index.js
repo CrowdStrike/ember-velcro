50cc8bc2118ea1202ad24f200b957d728fab2b51
Cam Skene <camskene@gmail.com>
2021-10-30 18:34:13 -0400

    feat: create popper and yield modifiers

--- addon/components/velcro/index.js
+++ addon/components/velcro/index.js
@@ -1,3 +1,24 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { createPopper } from '@popperjs/core';
+import { modifier } from 'ember-modifier';
 
-export default class VelcroComponent extends Component {}
+export default class VelcroComponent extends Component {
+  _targetElement = undefined;
+  _popoverElement = undefined;
+
+  velcroTarget = modifier((element) => {
+    this._targetElement = element;
+  });
+
+  velcro = modifier((element) => {
+    this._popoverElement = element;
+
+    this._createPopper();
+  });
+
+  @action
+  _createPopper() {
+    createPopper(this._targetElement, this._popoverElement);
+  }
+}
